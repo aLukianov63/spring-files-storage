7e064ee1c287bce0003d043232e9a55fd42e65e7
alukianov <101193473+aLukianov63@users.noreply.github.com>
2023-09-10 20:03:27 +0400

    initial commit

---
+++ src/main/java/com/alukianov/FileServer/FileServerApplication.java
@@ -0,0 +1,13 @@
+package com.alukianov.FileServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alukianov/FileServer/FileServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alukianov.FileServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
